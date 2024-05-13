f24880325b33330aa0f1f8d79fb7e33af6a0356a
Vasily Yuryev <vasily.yuryev.a1@gmail.com>
2024-05-13 18:48:26 +0300

    upload

--- droplets_solver_1/solver.py
+++ droplets_solver_1/solver.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 from scipy.stats import lognorm
 
 
-MODE = 0 # 0 -- inf 1 -- one_third_rule(x) 2 -- one_third_rule(T)
-
+MODE =1 # 0 -- inf 1 -- one_third_rule(x) 2 -- one_third_rule(T)
+RM = False
 
 """
 x - [T, m]
@@ -17,109 +17,165 @@ x - [T, m]
 MILLI = 10**-3
 T_C0 = 273.15
 RGC = 8.31
+P = 10**5
 
-rho_l = 1000 # water
-c_l = 4200 # water
+
+# water
+rho_l = 1000 
+c_l = 4200 
+h_gl = 40.66 * 1000
+M = 18/1000
+SP = "H2O"
+mech = "gri30.yaml"
+SP_O2 = "O2"
+SP_N2 = "N2"
+
+# # Ethanol
+# rho_l = 1000 
+# c_l = 4200 
+# h_gl = 38.6 * 1000
+# M = 18/1000
+# SP = "c2h5oh"
+# mech = "/Users/vasily/Downloads/ethanol-marinov.yml"
+# SP_O2 = "o2"
+# SP_N2 = "n2"
+
+
+
+
 
 # water = ct.Water()
 # water.P_sat()
-v = 0.2 # m/s
+v = 1 # m/s
 T_inf = 30 + T_C0
-T_0 = 30 + T_C0
-M = 18/1000
-h_gl = 40.66 * 1000
+T_0 = 68 + T_C0
+
+
 L = 0.02
 
-ct_inf_sol = ct.Solution('gri30.yaml')
-ct_s_d = ct.Solution('gri30.yaml')
-ct_m_d = ct.Solution('gri30.yaml')
-ct_inf_sol.TPX = T_inf, 10**5, {"N2" : 0.78, "O2": 0.2, "H2O": 0.02}
-H2O_ind = ct_inf_sol.species_index("H2O")
+Y_inf = 0.001
 
-C_inf = ct_inf_sol.concentrations[H2O_ind]*1000
+ct_inf_sol = ct.Solution(mech)
+ct_s_d = ct.Solution(mech)
+ct_m_d = ct.Solution(mech)
+
+
+Y_air_N2 = 0.8
+Y_air_O2 = 0.2
+
+
+def calc_Y(Y_SP):
+    return {SP_N2 : Y_air_N2*(1-Y_SP), SP_O2: Y_air_O2*(1-Y_SP), SP: Y_SP}
+
+
+ct_inf_sol.TPY = T_inf, P, calc_Y(Y_inf)
+SP_ind = ct_inf_sol.species_index(SP)
+
+C_inf = ct_inf_sol.concentrations[SP_ind]*1000
 mu_inf = ct_inf_sol.viscosity
 rho_inf = ct_inf_sol.density_mass
 cp_inf = ct_inf_sol.cp_mass
 k_inf = ct_inf_sol.thermal_conductivity
-D_inf = ct_inf_sol.mix_diff_coeffs[H2O_ind]
+D_inf = ct_inf_sol.mix_diff_coeffs[SP_ind]
 
 
 def one_third_rule(x_s, x_inf):
     return x_s + 1/3 * (x_inf - x_s)
 
 def x_dot(x, t):
+    # global RM
     T, m = x
+
+
     Tc = T - 273.15
     
     P_sat =  1000 * 0.61094 * np.exp((17.625*Tc) / (Tc + 243.04))
     C_s = P_sat / (T * RGC)
+
     
     # from cantera
 
-
-
-    
-    C_all = 10**5 / (T * RGC)
-    
+    # print(P_sat, P)
 
     
-    if MODE == 2:
-        ct_m_d.TPX = one_third_rule(T, T_inf), 10**5,  {"N2": one_third_rule(0.8*(C_all - C_s), 0.78), "O2": one_third_rule(0.2*(C_all - C_s), 0.2), "H2O": one_third_rule(C_s, 0.02)}
+    C_all = P / (T * RGC)
+    
+    Y_s = C_s*M /  (  (C_all - C_s) * (24 * Y_air_N2 + Y_air_O2*32)/1000 + C_s*M )
 
-        C_m_ = ct_m_d.concentrations[H2O_ind]*1000
-        # print(C_s_, C_s)
-        mu_m = ct_m_d.viscosity
-        rho_m = ct_m_d.density_mass
-        cp_m = ct_m_d.cp_mass
-        k_m = ct_m_d.thermal_conductivity
-        D_m = ct_m_d.mix_diff_coeffs[H2O_ind]
+    # print("Y_s", Y_s)
+
+    
+
+    
+    # if MODE == 2:
+    #     ct_m_d.TPX = one_third_rule(T, T_inf), P,  {SP_N2: one_third_rule(0.8*(C_all - C_s), 0.78), SP_O2: one_third_rule(0.2*(C_all - C_s), 0.2), "SP": one_third_rule(C_s, 0.02)}
+
+    #     C_m_ = ct_m_d.concentrations[SP_ind]*1000
+    #     # print(C_s_, C_s)
+    #     mu_m = ct_m_d.viscosity
+    #     rho_m = ct_m_d.density_mass
+    #     cp_m = ct_m_d.cp_mass
+    #     k_m = ct_m_d.thermal_conductivity
+    #     D_m = ct_m_d.mix_diff_coeffs[SP_ind]
 
     if MODE == 1:
-        ct_s_d.TPX =T, 10**5,  {"N2": 0.8*(C_all - C_s), "O2": 0.2*(C_all - C_s), "H2O": C_s}
-        C_s_ = ct_s_d.concentrations[H2O_ind]*1000
+        ct_s_d.TPY =T, P, calc_Y(Y_s)
+        C_s_ = ct_s_d.concentrations[SP_ind]*1000
         # print(C_s_, C_s)
         mu_s = ct_s_d.viscosity
         rho_s = ct_s_d.density_mass
         cp_s = ct_s_d.cp_mass
         k_s = ct_s_d.thermal_conductivity
-        D_s = ct_s_d.mix_diff_coeffs[H2O_ind]
+        D_s = ct_s_d.mix_diff_coeffs[SP_ind]
         mu_m = one_third_rule(mu_s, mu_inf) 
         rho_m = one_third_rule(rho_s, rho_inf) 
         cp_m = one_third_rule(cp_s, cp_inf) 
         k_m = one_third_rule(k_s, k_inf) 
         D_m = one_third_rule(D_s,D_inf) 
 
-    if MODE == 0:
-        mu_m =mu_inf
-        rho_m =rho_inf
-        cp_m = cp_inf
-        k_m = k_inf
-        D_m = D_inf
+    # if MODE == 0:
+    #     mu_m =mu_inf
+    #     rho_m =rho_inf
+    #     cp_m = cp_inf
+    #     k_m = k_inf
+    #     D_m = D_inf
 
 
     
     
     
     d = np.cbrt(6*m/rho_l/np.pi)
+    if d < 1e-6:
+        return np.zeros((2))
 
     Re = rho_m*d*v/mu_m
     Pr = cp_m*mu_m/k_m
     Sc = mu_m/(rho_m * D_m)
+    B_m = (Y_inf - Y_s)/(Y_s - 1)
+    if RM:
+        # Ranz Marshal ?
+        Nu = 2 + 0.6*np.sqrt(Re)*np.cbrt(Pr)
+        Sh = 2 + 0.6*np.sqrt(Re)*np.cbrt(Sc)
 
-    # Ranz Marshal ?
-    Nu = 2 + 0.6*np.sqrt(Re)*np.cbrt(Pr)
-    Sh = 2 + 0.6*np.sqrt(Re)*np.cbrt(Sc)
 
+    
 
+        q = (Nu *k_m) * np.pi * d * (T_inf - T)
+        g = (Sh*D_m) * np.pi * d * rho_m * np.log(1+B_m)
 
-
-    q = (Nu *k_m) * np.pi * d * (T - T_inf)
-    g = M*(Sh*D_m) * np.pi * d * (C_s - C_inf)
+    else:
+       
+        # print("B_m", B_m)
+        g = 2*np.pi * d * D_m * rho_m * np.log(1+B_m)
+        # B_T = B_m
+        q = 2*np.pi * d * k_m * (T_inf - T) 
 
     # print(C_s, C_inf)
     
     dm = -g
-    dT = -q/m/c_l + (h_gl/c_l)*dm/m
+    dT = q/m/c_l + (h_gl/M/c_l)*dm/m
+
+    
 
     
     return np.array([
@@ -127,43 +183,45 @@ def x_dot(x, t):
         dm
     ])
 
-def solve_for_d(D_0):
+def solve_for_d(D_0, t):
     
     m_0 = rho_l * D_0**3 * np.pi * 1/6
 
     x0 = np.array([T_0, m_0])
     # print(x0)
-    ts = np.linspace(0, L/v, 100)
+    ts = np.linspace(0, t, 100)
     sol = scipy.integrate.odeint(x_dot, x0, ts)
     m = sol[:, 1]
     T = sol[:, 0]
     d = np.cbrt(6*m/rho_l/np.pi)
     # print(d[0], d[-1])
     return ts, T, d
-D0 = 0.1*10**-3
-MODE = 0
-ts_m0, T_m0, d_m0 = solve_for_d(D0)
+D0 = 5*10**-3
 MODE = 1
-ts_m1, T_m1, d_m1 = solve_for_d(D0)
-MODE = 2
-ts_m2, T_m2, d_m2 = solve_for_d(D0)
+RM = False
+ts_m0, T_m0, d_m0 = solve_for_d(D0, 5*60)
+MODE = 1
+RM = True
+ts_m1, T_m1, d_m1 = solve_for_d(D0, 5*60)
+# MODE = 2
+# ts_m2, T_m2, d_m2 = solve_for_d(D0)
 
 # # print(sol)
 plt.figure()
 plt.plot(ts_m0, T_m0, label="Temp, K 0")
-plt.plot(ts_m1, T_m1, label="Temp, K 1")
-plt.plot(ts_m2, T_m2, label="Temp, K 2")
+plt.plot(ts_m1, T_m1, label="Temp, K 1 RM")
+# plt.plot(ts_m2, T_m2, label="Temp, K 2")
 plt.legend()
 
 plt.figure()
-plt.plot(ts_m0, d_m0*1000, label="diameter, mm 0")
-plt.plot(ts_m1, d_m1*1000, label="diameter, mm 1")
-plt.plot(ts_m2, d_m2*1000, label="diameter, mm 2")
+plt.plot(ts_m0, (d_m0*1000)**3/2, label="diameter^3/2, mm 0")
+plt.plot(ts_m1, (d_m1*1000)**3/2, label="diameter^3/2, mm 1 RM")
+# plt.plot(ts_m2, d_m2*1000, label="diameter, mm 2")
 plt.legend()
-# plt.show()
+plt.show()
 # exit(0)
 
-
+RM = True
 # distr = lognorm(s=0.8, scale=0.2)
 # ds = np.linspace(0, 1)
 # plt.figure()
@@ -173,13 +231,24 @@ plt.legend()
 
 T_19 = np.load("/Users/vasily/Downloads/Telegram Desktop/68/18/Rs.npy") / 150 /1000
 T_16 = np.load("/Users/vasily/Downloads/Telegram Desktop/68/16/Rs.npy") / 150 /1000
+
+th = 0.05*1e-3
+print(th, th)
+
+
+
+
+# print(np.quantile(T_16, 0.1))
 # print(np.min(T_16), np.min(T_19))
 
 
 density = True
 
-d_T_out = np.array([(lambda res: [res[2][-2], res[1][-2]])(solve_for_d(d)) for d in T_19])
-d_T_out = d_T_out[d_T_out[:, 0] > np.min(T_16)]
+d_T_out = np.array([(lambda res: [res[2][-2], res[1][-2]])(solve_for_d(d, L/v)) for d in T_19])
+d_T_out = d_T_out[d_T_out[:, 0] > th]
+
+T_19 = T_19[T_19 > th]
+T_16 = T_16[T_16 > th]
 plt.figure()
 plt.hist(T_19*1000, bins=np.linspace(0,1, 30), density=density, color="g", alpha=0.5, label="before", histtype=u'step', linewidth=2)
 plt.hist(d_T_out[:, 0]*1000, bins=np.linspace(0,1, 30), density=density, color="r", alpha=0.5, label="after sim", histtype=u'step', linewidth=2)
---
+++ l/proc.py
@@ -0,0 +1,170 @@
+import cv2
+import numpy as np
+import pickle 
+
+
+
+path = "/Users/vasily/Downloads/Telegram Desktop/VID_20240410_204750.mp4"
+
+
+def _nt(p: np.ndarray) -> tuple:
+    return (int(p[0]), int(p[1]))
+cap = cv2.VideoCapture(path)
+
+track_point = np.zeros(2,dtype=int)
+blink_point = np.ones(2,dtype=int)*3
+init_point = np.zeros(2, dtype=int)
+current_pnt = np.zeros(2, dtype=int)
+init_mode = False
+play = False
+
+
+def callback(event, x, y, flags, params):
+    global blink_point, track_point, init_mode, play
+    # print(pnts, selected_i, event)
+    if not play:
+        if not init_mode:
+            if event == cv2.EVENT_RBUTTONDOWN:
+                blink_point[0] = x
+                blink_point[1] = y
+            if event == cv2.EVENT_LBUTTONDOWN:
+                track_point[0] = x
+                track_point[1] = y
+        else:
+            if event == cv2.EVENT_RBUTTONDOWN:
+                init_point[0] = x
+                init_point[1] = y
+
+# ret, frame = cap.read()
+cv2.namedWindow("cimg")
+# cv2.waitKey(1)
+cv2.setMouseCallback("cimg", callback)
+
+i = 0
+# g_kksize = 11
+l_kksize = 11
+
+
+
+img0 = None
+
+N = 600
+
+ab_s = []
+while True:
+    if (not play  and i < N) or play:
+        ret, img = cap.read()
+        i+=1
+    else:
+        img = img0.copy()
+    if not ret:
+        break
+
+   
+    if i == N and img0 is None:
+        img0 = img
+    if i >= N:
+
+        print(i)
+        crop = img[track_point[1]:blink_point[1], track_point[0]:blink_point[0], :]
+        gray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY).astype(np.float32)/255.
+        # # blur = cv2.GaussianBlur(gray, (g_kksize,g_kksize), 0)
+
+        dx = cv2.Sobel(gray, cv2.CV_32F, 1, 0, ksize=l_kksize)/(2**(l_kksize*2 -1-0-2))
+        dy = cv2.Sobel(gray, cv2.CV_32F, 0, 1, ksize=l_kksize)/(2**(l_kksize*2 -1-0-2))
+        sobel_abs = np.sqrt(np.power(dx, 2) + np.power(dy, 2))
+        maskb = sobel_abs > 0.045
+        # mask = ((maskb)*255).astype("uint8")
+        
+        ys, xs = np.where(maskb)
+
+        pnts = np.zeros((len(ys), 2))
+        pnts[:, 0] = xs
+        pnts[:, 1] = ys
+        pnts  = pnts
+        cur_pnts = pnts[np.linalg.norm(pnts - current_pnt, axis=1) <  50, :]
+        
+       
+
+        
+        update_pnt = np.mean(cur_pnts, axis=0)
+        print(update_pnt)
+
+        for p in cur_pnts:
+            cv2.circle(crop, _nt(p), 1, (0, 0, 255), -1)
+        
+        # m2 = mask
+        # m2 = cv2.morphologyEx(m2, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3)),iterations = 1)
+        # # sobel_abs = m2 * (sobel_abs)
+        # m2 = cv2.dilate(m2,cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(11,11)),iterations = 1)
+        
+
+
+
+        
+
+        # # cv2.imshow("img", img)
+        
+        # cv2.imshow("m2", m2)
+        
+
+        cv2.circle(img, _nt(track_point), 10, (255, 0, 255), 4)
+        cv2.circle(img, _nt(blink_point), 10, (255, 255, 0), 4)
+        cv2.circle(img, _nt(init_point), 10, (0, 255, 0), 4)
+        if update_pnt is not None and len(update_pnt) > 0 and not np.isnan(update_pnt[0]):
+            cv2.circle(img, _nt(update_pnt + track_point), 10, (0, 0, 255), 4)
+            if play:
+                current_pnt = update_pnt.copy()
+            # q = 0.08
+            # x1 = np.quantile(cur_pnts[:, 0], q).astype(int)
+            # x2 = np.quantile(cur_pnts[:, 0], 1-q).astype(int)
+            # y1 = np.quantile(cur_pnts[:, 1], q).astype(int)
+            # y2 = np.quantile(cur_pnts[:, 1], 1-q).astype(int)
+            # cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2) 
+            x = cur_pnts[:, 0]
+            y = cur_pnts[:, 1]
+            A = np.vstack([x**2, x * y, y**2, x, y]).T
+            b = np.ones((len(x), 1))
+            x = (np.linalg.pinv(A) @ b ).flatten()
+
+            A, B, C, D, E = -x
+            F = 1
+            a = -np.sqrt(2*(A*E**2 + C*D**2 - B*D*E + (B**2 - 4*A*C)*F)*((A+C) + np.sqrt((A-C)**2 +B**2)) )/(B**2 - 4*A*C)
+            b = -np.sqrt(2*(A*E**2 + C*D**2 - B*D*E + (B**2 - 4*A*C)*F)*((A+C) - np.sqrt((A-C)**2 +B**2)) )/(B**2 - 4*A*C)
+            if play:
+                ab_s.append((a,b))
+            # print(a*b*min(a,b) *np.pi *4/3)
+            # for p in fit:
+                # cv2.circle(crop, _nt(p), 1, (255, 0, 0), -1)
+
+
+
+
+
+        cv2.circle(img, _nt(current_pnt + track_point), 10, (255, 0, 0), 4)
+
+        cv2.imshow("cimg", img)
+
+        cv2.imshow("crop", crop)
+        cv2.imshow("sobel_abs", maskb*sobel_abs*20)
+
+
+        k = cv2.waitKey(10)
+        if k == ord("q"):
+            break
+        if k == ord("p"):
+            play = True
+        if k == ord("i"):
+            init_mode = not init_mode
+        if k == ord("s"):
+            pickle.dump((track_point, blink_point), open("points.pickle", "wb"))
+            pickle.dump(init_point, open("init_point.pickle", "wb"))
+        if k == ord("l"):
+            (track_point, blink_point) = pickle.load(open("points.pickle", "rb"))
+            track_point = track_point.astype(int)
+            blink_point = blink_point.astype(int)
+            init_point = pickle.load(open("init_point.pickle", "rb"))
+            current_pnt = init_point.copy() - track_point
+
+
+pickle.dump(ab_s, open("ab_s.pickle", "wb"))
