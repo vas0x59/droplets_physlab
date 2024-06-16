6d9616bbfd549c154d0774b30335215e2cc8830a
vas0x59 <vasily.yuryev.a1@gmail.com>
2024-06-16 18:36:48 +0300

    pbe solver

---
+++ julia_solver/solver.jl
@@ -0,0 +1,41 @@
+using ModelingToolkit, MethodOfLines, DomainSets, NonlinearSolve
+
+@parameters x y
+@variables u(..)
+Dxx = Differential(x)^2
+Dyy = Differential(y)^2
+
+Dx = Differential(x)
+Dy = Differential(y)
+
+eq = Dxx(u(x, y)) + Dyy(u(x, y)) ~ 0
+
+bcs = [u(0, y) ~ x * y,
+       u(1, y) ~ x * y,
+       u(x, 0) ~ x * y,
+       u(x, 1) ~ x * y]
+
+
+# Space and time domains
+domains = [x ∈ Interval(0.0, 1.0),
+           y ∈ Interval(0.0, 1.0)]
+
+@named pdesys = PDESystem([eq], bcs, domains, [x, y], [u(x, y)])
+
+dx = 0.2
+dy = 0.2
+
+# Note that we pass in `nothing` for the time variable `t` here since we
+# are creating a stationary problem without a dependence on time, only space.
+discretization = MOLFiniteDifference([x => dx, y => dy], nothing, approx_order=2)
+
+prob = discretize(pdesys, discretization)
+sol = NonlinearSolve.solve(prob, NewtonRaphson())
+
+u_sol = sol[u(x, y)]
+
+using Plots
+
+heatmap(sol[x], sol[y], u_sol, xlabel="x values", ylabel="y values",
+        title="Steady State Heat Equation")
+savefig("fig.png")
